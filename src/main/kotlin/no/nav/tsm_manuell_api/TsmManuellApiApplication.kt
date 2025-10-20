178bcdfc99cf77f18a334222787c67b4e473330a
Natalie Uranes <natalie.uranes@nav.no>
2025-10-20 17:19:46 +0200

    chore: add health chechs
    - add spring health checks
    - change health chech paths for kubernetes

--- src/main/kotlin/no/nav/tsm_manuell_api/TsmManuellApiApplication.kt
+++ src/main/kotlin/no/nav/tsm_manuell_api/TsmManuellApiApplication.kt
@@ -3,9 +3,8 @@ package no.nav.tsm_manuell_api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class TsmManuellApiApplication
+@SpringBootApplication class TsmManuellApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<TsmManuellApiApplication>(*args)
+    runApplication<TsmManuellApiApplication>(*args)
 }
