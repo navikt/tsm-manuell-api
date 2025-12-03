33b18adffca1ad3e0f55a00d965f2ea49618be92
Jørn-Are <ja.flaten91@gmail.com>
2025-12-03 15:14:51 +0100

    chore: added extension for pgaudit

--- src/main/resources/db/migration/V2__Disable_auditlog_for_app_user.sql
+++ src/main/resources/db/migration/V2__Disable_auditlog_for_app_user.sql
@@ -1,11 +1,14 @@
-CREATE EXTENSION IF NOT EXISTS pgaudit;
 DO
 $$
 BEGIN
-        IF EXISTS
-            (SELECT 1 from pg_roles where rolname = 'tsm-manuell-api-instance')
-        THEN
+    -- Only create extension if it's available
+    IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'pgaudit') THEN
+        CREATE EXTENSION IF NOT EXISTS pgaudit;
+        
+        -- Configure the user if it exists
+        IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'tsm-manuell-api-instance') THEN
             ALTER USER "tsm-manuell-api-instance" IN DATABASE "tsm-manuell-api" SET pgaudit.log TO 'none';
-END IF;
+        END IF;
+    END IF;
 END
 $$;
