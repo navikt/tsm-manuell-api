4b0b7215f653bbc704a134edefebc412a509d8cb
Jørn-Are <ja.flaten91@gmail.com>
2025-12-03 12:22:49 +0100

    chore: updated v2 migration script

--- src/main/resources/db/migration/V2__Disable_auditlog_for_app_user.sql
+++ src/main/resources/db/migration/V2__Disable_auditlog_for_app_user.sql
@@ -4,15 +4,7 @@ BEGIN
         IF EXISTS
             (SELECT 1 from pg_roles where rolname = 'tsm-manuell-api-instance')
         THEN
-            BEGIN
-                ALTER USER "tsm-manuell-api-instance" IN DATABASE "tsm-manuell-api" SET pgaudit.log TO 'none';
-                RAISE NOTICE 'Successfully disabled pgaudit for tsm-manuell-api-instance';
-            EXCEPTION
-                WHEN insufficient_privilege THEN
-                    RAISE WARNING 'Cannot set pgaudit.log: insufficient privileges. This is expected in dev/test environments.';
-                WHEN OTHERS THEN
-                    RAISE WARNING 'Cannot set pgaudit.log: % (SQLSTATE: %). This is expected if pgaudit extension is not installed.', SQLERRM, SQLSTATE;
-            END;
+            ALTER USER "tsm-manuell-api-instance" IN DATABASE "tsm-manuell-api" SET pgaudit.log TO 'none';
 END IF;
 END
 $$;
