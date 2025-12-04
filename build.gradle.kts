274dc5a347cb4b67f52f8f84955d9393ea9b8d7a
Jørn-Are <ja.flaten91@gmail.com>
2025-12-04 16:17:27 +0100

    chore: added restclient dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-restclient")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
