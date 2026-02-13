85dd0d4f3c037c31d6f889fb0e2140d7ffd36974
Jørn-Are <ja.flaten91@gmail.com>
2026-02-13 07:52:29 +0100

    chore: upgraded spotless to 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 	kotlin("plugin.spring") version "2.3.0"
 	id("org.springframework.boot") version "4.0.2"
 	id("io.spring.dependency-management") version "1.1.7"
-    id("com.diffplug.spotless") version "8.0.0"
+    id("com.diffplug.spotless") version "8.2.1"
 }
 
 java {
