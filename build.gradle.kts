95bfb426e7d45328e519d70a1746b686c5cb6597
Jørn-Are <ja.flaten91@gmail.com>
2026-02-13 07:46:00 +0100

    chore: spring boot 4.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val caffeineVersion = "3.2.3"
 plugins {
 	kotlin("jvm") version "2.3.0"
 	kotlin("plugin.spring") version "2.3.0"
-	id("org.springframework.boot") version "4.0.0"
+	id("org.springframework.boot") version "4.0.2"
 	id("io.spring.dependency-management") version "1.1.7"
     id("com.diffplug.spotless") version "8.0.0"
 }
