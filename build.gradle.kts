1b9d3c50b6f7e75834e89e8a0931c4819ce965bc
Jørn-Are <ja.flaten91@gmail.com>
2026-02-13 11:09:08 +0100

    chore: gradle 9.3.1 and testcontainers 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,9 +54,9 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.kafka:spring-kafka-test")
-    testImplementation("org.testcontainers:testcontainers:1.21.3")
-    testImplementation("org.testcontainers:postgresql:1.21.3")
-    testImplementation("org.testcontainers:junit-jupiter:1.21.3")
+    testImplementation("org.testcontainers:testcontainers-kafka:2.0.1")
+    testImplementation("org.testcontainers:testcontainers-postgresql:2.0.1")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter:2.0.1")
     testImplementation("org.mockito:mockito-inline:5.2.0")
 
     runtimeOnly("org.postgresql:postgresql")
